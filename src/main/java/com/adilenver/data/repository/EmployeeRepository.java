239c5a8c132d68dd993bc3f2bd3c25c2cd6b372e
Adil Enver <Adiljan.anwar@gmail.com>
2023-01-30 11:58:41 +0300

    Success All(Create-Delete-Update-List-Find)

--- src/main/java/com/adilenver/data/repository/EmployeeRepository.java
+++ src/main/java/com/adilenver/data/repository/EmployeeRepository.java
@@ -1,9 +1,10 @@
 package com.adilenver.data.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import com.adilenver.data.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 //JpaRepository > CrudRepository
 @Repository
-public interface EmployeeRepository extends CrudRepository {
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
 }
--- src/main/java/com/adilenver/utils/PermaLink.java
+++ src/main/java/com/adilenver/utils/PermaLink.java
@@ -2,4 +2,7 @@ package com.adilenver.utils;
 
 public class PermaLink {
     //Github: https://github.com/AdilEnverUygur/SpringBootKodluyoruz
+    //  SWAGGER: http://localhost:8080/swagger-ui/index.html
+    //  H2-console: http://localhost:8080/h2-console/
+    //  JSON Online parser:  https://jsoneditoronline.org/#left=local.gesomi&right=local.revibe
 }
